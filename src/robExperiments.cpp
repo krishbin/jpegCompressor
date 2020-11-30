c0c688c08fb592cb062b87b3cbcd7225d66a1ae5
FluxO4 <46083008+FluxO4@users.noreply.github.com>
2020-11-30 13:17:22 +0545

    entropy coding table

--- src/robExperiments.cpp
+++ src/robExperiments.cpp
@@ -36,7 +36,7 @@ public:
 
 static class ChunkTransformer {
 private:
-	int compressionWeights50values[8][8] = {
+	int YquantizationTableValues50[8][8] = {
 		{16,12,14,14,18,24,49,72},
 		{11,12,13,17,22,35,64,92},
 		{10,14,16,22,37,55,78,95},
@@ -45,6 +45,28 @@ private:
 		{40,58,57,87,109,104,121,100},
 		{51,60,69,80,103,113,120,103},
 		{61,55,56,62,77,92,101,99}
+	};
+
+	int CquantizationTableValues50[8][8] = {
+		{17,18,24,47,99,99,99,99},
+		{18,21,26,66,99,99,99,99},
+		{24,26,56,99,99,99,99,99},
+		{47,66,99,99,99,99,99,99},
+		{99,99,99,99,99,99,99,99},
+		{99,99,99,99,99,99,99,99},
+		{99,99,99,99,99,99,99,99},
+		{99,99,99,99,99,99,99,99}
+	};
+
+	int entropyEncodingTable[8][8] = {
+		{0,1,5,6,14,15,27,28},
+		{2,4,7,13,16,26,29,42},
+		{3,8,12,17,25,30,41,43},
+		{9,11,18,24,31,40,44,53},
+		{10,19,23,32,39,45,52,54},
+		{20,22,33,38,46,51,55,60},
+		{21,34,37,47,50,56,59,61},
+		{35,36,48,49,57,58,62,63}
 	};
 
 	float alpha(int u) {
@@ -56,7 +78,8 @@ private:
 		}
 	}
 public:
-	const Chunk<int> compressionWeights50 = Chunk<int>(compressionWeights50values);
+	const Chunk<int> YquantizationTable = Chunk<int>(YquantizationTableValues50);
+	const Chunk<int> CquantizationTable = Chunk<int>(CquantizationTableValues50);
 
 	Chunk<float> DCT(Chunk<int> pixelValues) {
 		float result[8][8] = {
@@ -112,7 +135,14 @@ public:
 					}
 				}
 				DCTresult[k1][k2] *= 0.25 * alpha(k1) * alpha(k2);
-				result[k1][k2] = (int)round(DCTresult[k1][k2] / (weights.pixelValues[k1][k2] * 1.0f));
+				result[k1][k2] = (int)round(DCTresult[k1][k2] / (weights.pixelValues[k1][k2] * 1.0f)) + 128;
+				
+				if (result[k1][k2] > 255) {
+					result[k1][k2] = 255;
+				}
+				if (result[k1][k2] < 0) {
+					result[k1][k2] = 0;
+				}
 			}
 		}
 
@@ -154,8 +184,8 @@ public:
 				}
 				iresult[k1][k2] = (int)round(result[k1][k2] * 0.25) + 128;
 				
-				if (iresult[k1][k2] > 256) {
-					iresult[k1][k2] = 256;
+				if (iresult[k1][k2] > 255) {
+					iresult[k1][k2] = 255;
 				}
 				if (iresult[k1][k2] < 0) {
 					iresult[k1][k2] = 0;
@@ -170,11 +200,14 @@ public:
 	string ConvertToBinString(Chunk<int> compressed) {
 		int intString[64];
 
-		//Entropy coding
 
-		for (int i = 0; i < 64; i++) {
-
+		for (int i1 = 0; i1 < 64; i1++) {
+			for (int i2 = 0; i2 < 64; i2++) {
+				intString[entropyEncodingTable[i1][i2]] = compressed.pixelValues[i1][i2];
+			}
 		}
+
+		//Translate this to a string somehow
 		return "abcde";
 	}
 
